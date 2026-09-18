7b88adc79a90d87ef48bdd4c5407d7cfd4c3949f
itsmanikandan73 <soulquiet8@gmail.com>
2026-09-18 08:48:41 +0530

    adding cards_six.c program

---
+++ Heads-First-C/cards_six.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* modified version of the code | cards game */
+
+int main(){
+	
+	char card_name[3];
+
+	puts("Enter the card name");
+	scanf("%2s", card_name);
+
+	int val = 0;
+	if (card_name[0] == 'K'){
+		val = 10;
+	}
+	else if(card_name[0] == 'Q'){
+		val = 10;
+	}
+	else if(card_name[0] == 'J'){
+		val = 10;
+	}
+	else if(card_name[0] == 'A'){
+		val = 11;
+	}
+	else {
+		val = atoi(card_name);
+	}
+	// check if the value is 3 to 6 
+	if((val > 2) && (val < 7)){
+		puts("Count has gone up");
+		// otherwise check if the card was 10 J, Q or K
+	}
+	else if( val == 10){
+		puts ("Count has gone down");
+	}
+
+	return 0;
+
+}
