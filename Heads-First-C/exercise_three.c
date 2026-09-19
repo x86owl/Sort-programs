0add41a6be404e646b594ed65c6b1ff6b28ea681
itsmanikandan73 <soulquiet8@gmail.com>
2026-09-19 20:38:48 +0530

    adding exercise_three.c program

---
+++ Heads-First-C/exercise_three.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(){
+	
+	char ex[20];
+	
+	puts("Enter your girlfriend's name: ");
+	scanf("%19s", ex);
+
+	printf("Dear %s.\n\n\t You are my history.\n", ex);
+
+	return 0;
+}
