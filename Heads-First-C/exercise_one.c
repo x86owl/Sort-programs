033c2b8ee171fb87f8a611368ba43d095a37ebf8
itsmanikandan73 <soulquiet8@gmail.com>
2026-09-19 20:37:45 +0530

    adding exercise_one.c program

---
+++ Heads-First-C/exercise_one.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ This is comment section to describe the code 
+ 1. Define an interger variable named month_count with value 12
+ 2. if month_count is more than 6
+ 3. we display the message 
+ */
+
+int main(){
+	
+	int month_count = 12;
+
+	if (month_count > 6){
+		puts("Code everyday,To become a good programmer.");   // why puts() instead of printf() | just search online ask to the LLM.
+
+	}
+	
+	return 0;
+}
